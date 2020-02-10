eb16f4dfc6b0172a428aa9b373e9076ca422bd03
Tomofumi Kusana <tkusana@morisawa.co.jp>
2020-02-10 11:21:32 +0900

    sessionが取れなかった場合のエラーハンドリングを追加

--- 01-Login/routes/login/login.go
+++ 01-Login/routes/login/login.go
@@ -19,11 +19,12 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	state := base64.StdEncoding.EncodeToString(b)
 
-	session, _ := app.Store.Get(r, "auth-session")
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
+	session, err := app.Store.Get(r, "auth-session")
+	if session == nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	session.Values["state"] = state
 	err = session.Save(r, w)
 	if err != nil {
